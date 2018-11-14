f8e4dea8362f41bac4a642a4182291587ffbc42e
Marcos Venicius Ubiali <ubiali.marcosvenicius@gmail.com>
2018-11-14 09:41:20 -0200

    initial configuration

--- config.sample.js
+++ config.sample.js
@@ -1,10 +1,10 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
-    FB_APP_SECRET: '',
-    SERVER_URL: '',
-    GOOGLE_PROJECT_ID: '',
-    DF_LANGUAGE_CODE: '',
-    GOOGLE_CLIENT_EMAIL: '',
-    GOOGLE_PRIVATE_KEY: ''
+    FB_PAGE_TOKEN: process.env.FB_PAGE_TOKEN,
+    FB_VERIFY_TOKEN: process.env.FB_VERIFY_TOKEN,
+    FB_APP_SECRET: process.env.FB_APP_SECRET,
+    SERVER_URL: process.env.SERVER_URL,
+    GOOGLE_PROJECT_ID: process.env.GOOGLE_PROJECT_ID,
+    DF_LANGUAGE_CODE: process.env.DF_LANGUAGE_CODE,
+    GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL,
+    GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY
 };
