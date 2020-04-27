a25f072dc0aa78bd99491b35da9928fc5496cfc8
Moayyadkh <31673360+Moayyadkh@users.noreply.github.com>
2020-04-27 21:34:01 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     FB_PAGE_TOKEN: 'EAAJ7mr8MuSIBAKH7yw3I60qbaiAvCrX9uNxFVZBGoxh5yPjVtAqYWK8DgvUTRZBEDAeagOBo33zVSTPesdp6L0SWk9l3hURHdCeLH79C13S6KufoVvQluBZBZCbHt0UsXyxSSfBj9OqzOlRtgrphX2ZAXnGafRcEyzO39VG3O8QZDZD',
     FB_VERIFY_TOKEN: 'MyFB_Heroku_app',
     FB_APP_SECRET: '',
-    SERVER_URL: '',
+    SERVER_URL: 'https://moayyadapp.herokuapp.com/webhook',
     GOOGLE_PROJECT_ID: '',
     DF_LANGUAGE_CODE: '',
     GOOGLE_CLIENT_EMAIL: '',
